8e9a0c002bafa2bc91bfb50be21be8e47d6dd9db
Ryan Wong <ryanwong@kaopubao.com>
2014-12-10 17:02:34 +0800

    base controller and view done

--- app/controllers/quick_admin/base_controller.rb
+++ app/controllers/quick_admin/base_controller.rb
@@ -1,13 +1,19 @@
 class QuickAdmin::BaseController < QuickAdmin.parent_controller.constantize
-  inherit_resources
   respond_to :html, :js, :json
 
-  def collection
-    get_collection_ivar || begin
-      @grid = resource_collection_name.to_s.camelize.constantize.new(params[resource_collection_name])
-      @assets = @grid.assets.page(params[:page])
-      set_collection_ivar @assets
+  module InstanceMethods
+    def collection
+      get_collection_ivar || begin
+        @grid = resource_collection_name.to_s.camelize.constantize.new(params[resource_collection_name])
+        @assets = @grid.assets.page(params[:page])
+        set_collection_ivar @assets
+      end
     end
+  end
+
+  def self.inherited klass
+    klass.inherit_resources
+    klass.include InstanceMethods
   end
 
 end
