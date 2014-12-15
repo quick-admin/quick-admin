6f1835881ff6b1c9cbfe10aae7b3b762d1d4fef0
陈阳 <chenyang@kaopubao.com>
2014-12-15 11:22:39 +0800

    fixes #1 根据Pundit是否在application_controller中定义决定authorize方法的加入

--- app/controllers/quick_admin/base_controller.rb
+++ app/controllers/quick_admin/base_controller.rb
@@ -2,18 +2,38 @@ class QuickAdmin::BaseController < QuickAdmin.parent_controller.constantize
   respond_to :html, :js, :json
 
   module InstanceMethods
+
     def collection
       get_collection_ivar || begin
         @grid = resource_collection_name.to_s.camelize.constantize.new(params[resource_collection_name])
         @assets = @grid.assets.page(params[:page])
+        authorize @assets if should_authorize?
         set_collection_ivar @assets
       end
+    end
+
+    def resource
+      @vendor_platform ||= proc do
+        authorize super if should_authorize?
+        super
+      end.call
+    end
+
+    def build_resource
+      @vendor_platform ||= proc do
+        authorize super if should_authorize?
+        super
+      end.call
     end
   end
 
   def self.inherited klass
     klass.inherit_resources
     klass.include InstanceMethods
+  end
+
+  def should_authorize?
+    ApplicationController.include?(Pundit) rescue false
   end
 
 end
