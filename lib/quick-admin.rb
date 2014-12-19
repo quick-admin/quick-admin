6d8ce5b4b9612a3fe3454641f1272feaea969371
Ryan Wong <ryanwong@kaopubao.com>
2014-12-19 11:29:33 +0800

    Add missing paramaters

--- lib/quick-admin.rb
+++ lib/quick-admin.rb
@@ -2,11 +2,18 @@ require 'rails'
 require 'kaminari'
 require 'inherited_resources'
 require 'simple_form'
+require 'nprogress-rails'
 
 module QuickAdmin
 
   mattr_accessor :parent_controller
   @@parent_controller = "ApplicationController"
+
+  mattr_accessor :parent_layout
+  @@parent_layout = "layouts/application"
+
+  mattr_accessor :root_path
+  @@root_path = "/"
 
 end
 
