7cde44309009ffc2dd6e3cf09979bf77574771e7
陈阳 <chenyang@kaopubao.com>
2015-02-02 12:38:25 +0800

    增加判断条件保证未使用标签页的其他页面正常，完善使用标签页的首页向其他未使用标签页的首页切换的处理

--- app/controllers/quick_admin/base_controller.rb
+++ app/controllers/quick_admin/base_controller.rb
@@ -9,14 +9,16 @@ class QuickAdmin::BaseController < QuickAdmin.parent_controller.constantize
         @assets = @grid.assets.page(params[:page])
         # params[scopes[:type]] will get the attribute's value like 'status' during the filtering
         @scope = params[:scope] || params[scopes[:type]] || scopes[:default_scope] rescue nil
-        unless scopes.blank?
+        controller_name = scopes[:controller] rescue nil
+        if !scopes.blank? && controller_name == self.class.name
           @assets = @assets.send(@scope)
           @type = scopes[:type]
           @scopes = scopes[:scopes]
+        else
+          @scope = nil
         end
         authorize @assets if should_authorize?
         set_collection_ivar @assets
-        scopes = nil
       end
     end
 
