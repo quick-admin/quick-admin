958b13a13e1937bbdc38c2b3b777b107966c8e60
Ryan Wong <ryanwong@kaopubao.com>
2015-06-05 20:48:32 +0800

    修正link_to 在model情况下参数缺失

--- app/helpers/quick_admin_helper.rb
+++ app/helpers/quick_admin_helper.rb
@@ -123,10 +123,10 @@ module QuickAdminHelper
     return super unless options.is_a?(ActiveRecord::Base)
     path =
       "#{ActiveModel::Naming.singular_route_key(options)}_path"
-    return link_to name, send(path, options) if respond_to?(path)
+    return link_to name, send(path, options), html_options if respond_to?(path)
     superclass = options.class.superclass
     path = "#{ActiveModel::Naming.singular_route_key(superclass)}_path"
-    return link_to name, send(path, options) if respond_to?(path)
+    return link_to name, send(path, options), html_options if respond_to?(path)
     super
   end
   # 对show页面显示actions动作面板
